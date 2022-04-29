1b3e8417f921d3b2470ed0eeaab9ff2a1cc495f1
gabirosales <gabrielarosales169@gmail.com>
2022-04-29 16:55:08 -0600

    Continuacion-CRUD

--- src/recursohumano/ConexionCRUD.java
+++ src/recursohumano/ConexionCRUD.java
@@ -15,4 +15,115 @@ public class ConexionCRUD {
     private final String driverConector = "com.msql.jdbc.Driver";
     //Objeto de la clase Connection del paquete de java.sql
     private static Connection conexion;
+
+public ConexionCRUD(){
+   try{
+        Class.forName(driverConector);
+        conexion=DriverManager.getConnection(servidor, usuario, clave);
+   }catch(ClassNotFoundException | SQLException e){
+       System.out.println("Conexion fallida! Error! :" + e.getMessage());
+
+} 
+
+
 }
+public Connection getConnection(){
+    return conexion;
+}
+
+
+public void guardarRegistros(String tabla, String camposTabla, String valoresCampos){
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+    try{
+        String sqlQueryStmt = "INSERT INTO " + "(" +camposTabla + ") VALUES (" + valoresCampos +");";
+        Statement stmt;
+        stmt = cone.createStatement();
+        stmt.executeUpdate(sqlQueryStmt);
+        
+        stmt.close();
+        cone.close();
+        System.out.println("Registro giardado correctamente!");
+    }catch(Exception e){
+        System.out.println(e.getMessage());
+    }
+
+    
+      
+}
+public void actualizarEliminarRegistro(String tabla, String valoresCamposNuevos, String Condicion){
+    
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+    try{
+        Statement stmt;
+        String sqlQueryStmt;
+        
+        if(valoresCamposNuevos.isEmpty()){
+            sqlQueryStmt = "DELETE FROM " + tabla + " WHERE " + Condicion + ""; 
+        }else{
+            sqlQueryStmt = "UPDATE " + tabla + " SET " + valoresCamposNuevos + " WHERE " + Condicion + ";"; 
+        }
+        stmt = cone.createStatement();
+        stmt.executeUpdate(sqlQueryStmt);
+        stmt.close();
+        cone.close();
+    }catch(SQLException ex){
+        System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage());
+        
+    }
+
+    }
+public void desplegarRegistros(String tablaBuscar, String camposBuscar, String condicionBuscar) throws SQLException{
+    
+    ConexionCRUD conectar = new ConexionCRUD();
+    Connection cone = conectar.getConnection();
+    try{
+        Statement stmt;
+        String sqlQueryStmt;
+        if(condicionBuscar.equals("")){
+            sqlQueryStmt = "SELECT " + camposBuscar + " FROM " + tablaBuscar + ";";
+        }else{
+            sqlQueryStmt = "SELECT " + camposBuscar + " FROM " + tablaBuscar + " WHERE " + condicionBuscar;
+        }
+        stmt = cone.createStatement();
+        stmt.executeQuery(sqlQueryStmt);
+        
+        try(ResultSet miResult = stmt.executeQuery(sqlQueryStmt)){
+            if (miResultSet.next()){
+                ResultSetMetaData metaData = miResultSet.getMetaData();
+                int numColumnas = metaData.getColumnCount();
+                System.out.println("<<REGISTROS ALMACENADOS>>");
+                System.out.println();
+                
+                for (int i = 1; i<=numColumnas; i++){
+                    System.out.printf("%-20s\t", metaData.getColumnName(i));
+                }
+                System.out.println();
+                do {
+                    for (int i = 1; i <= numColumnas; i++){
+                        System.out.printf("%-20s\t", miResultSet.getObject(i));
+                    }
+                    System.out.println();
+                }while (miResultSet.next());
+                System.out.println();
+            }else{
+                System.out.println("No se han encontrado registros");
+                         
+                }
+            miResultSet.close();
+        }finally{
+            stmt.close();
+            cone.close();
+            }
+    }catch (SQLException ex){
+        System.out.println("Ha ocurrido el siguiente error" + ex.getMessage());
+        }
+    }
+}
+
+
+
+
+
+
---
+++ src/recursohumano/miResultSet.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursohumano;
+
+import java.sql.ResultSetMetaData;
+
+/**
+ *
+ * @author gabi
+ */
+class miResultSet {
+
+    static boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static ResultSetMetaData getMetaData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object[] getObject(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
